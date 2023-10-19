7114ebf51fe4b55cdbbdb87082f3d9cb340b0139
Martin Povišer <povik@yosyshq.com>
2023-10-19 16:33:37 +0200

    Extend top with made-up 'perception correction'

--- top.v
+++ top.v
@@ -7,13 +7,37 @@ input wire [7:0] h;
 input wire [7:0] s;
 input wire [7:0] l;
 
-output wire rgb_valid;
-output wire [7:0] r;
-output wire [7:0] g;
-output wire [7:0] b;
+output reg rgb_valid;
+output reg [7:0] r;
+output reg [7:0] g;
+output reg [7:0] b;
 
-assign rgb_valid = hsl_valid;
-hsl_to_rgb #(.HUE_DEPTH(8))
-	conversion(.h(h), .s(s), .l(l), .r(r), .g(g), .b(b));
+reg [7:0] h_delay;
+reg [7:0] s_delay;
+reg hsl_delay_valid;
+wire [15:0] l_sq = l * l;
+reg [7:0] l_corrected;
+
+always @(posedge clk) begin
+	h_delay <= h;
+	s_delay <= s;
+	l_corrected = l_sq >> 8;
+	hsl_delay_valid <= hsl_valid;
+end
+
+wire [7:0] r_ahead;
+wire [7:0] g_ahead;
+wire [7:0] b_ahead;
+
+hsl_to_rgb #(.HUE_DEPTH(8), .RGB_DEPTH(8))
+	conversion(.h(h_delay), .s(s_delay), .l(l_corrected),
+			   .r(r_ahead), .g(g_ahead), .b(b_ahead));
+
+always @(posedge clk) begin
+	rgb_valid <= hsl_delay_valid;
+	r <= r_ahead;
+	g <= g_ahead;
+	b <= b_ahead;
+end
 
 endmodule
